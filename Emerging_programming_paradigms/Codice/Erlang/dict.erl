7f327a2329d326b8ed09fcdb7a57dfa85044806c
davidederosa <davide.derosa@studio.unibo.it>
2025-02-27 23:21:50 +0100

    terminata lezione 3 epp

--- Emerging_programming_paradigms/Codice/Erlang/dict.erl
+++ Emerging_programming_paradigms/Codice/Erlang/dict.erl
@@ -4,33 +4,36 @@
 % Alberi binari di ricerca con chiavi nei nodi interni e coppie 
 % chiavi-payload nelle foglie
 %
-% Esempio: Algebraic Data Type: tipo di dato che ha un certo numero 
+% Esempio Algebraic Data Type: tipo di dato che ha un certo numero 
 % finito di forme distinte dove ogni forma è un tipo di dato composto.
 %
-%   Tree K V ::= { leaf, K, V } | { node, Tree K V, K, Tree K V }
+% Un albero binario di ricerca è un esempio di dato algebrico, definito come:
+%       Tree K V ::= { leaf, K, V } | { node, Tree K V, K, Tree K V }
 %
-% Il | indica più possibili forme.
+% Il | indica più possibili forme. leaf e node indicano vengono aggiunti per 
+% discriminare le forme possibili. Nel nostro caso non sarebbe stato necessario, 
+% ma in casi dove la lunghezza delle tuple è uguale è un buon modo per 
+% differenziare più forme. Questo permette di avere codice più robusto.
 %
-% leaf e node indicano vengono aggiunti per discriminare le forme possibili.
-% Nel nostro caso non sarebbe stato necessario, ma in casi dove la lunghezza
-% delle tuple è uguale è un buon modo per differenziare più forme.
-% Questo permette di avere codice più robusto.
-
 % Esempio:
-%   { node, { leaf, 2, anna }, 4, { leaf, 5 , bruno} }
+%      { node, { leaf, 2, anna }, 4, { leaf, 5 , bruno} }
+%  
+%                             4
+%                           /   \
+%                { 2, anna }     { 5, bruno }
 %
-%                   4
-%                 /   \
-%      { 2, anna }     { 5, bruno }
 %
-% Tipo option:
-%   Option V ::= not_found | { found, V }
+% Tipo option, il quale permette di identificare se un valore è stato trovato 
+% o meno all'interno dell'albero. Viene definito come:
+%       Option V ::= not_found | { found, V }
+%
 %
 % Cerco un valore associato a una chiave in un albero binario di ricerca.
 % Implementazione della ricerca:
 
 search(K, {leaf, K2, V}) when K =:= K2 -> { found, V } ;
 search(_, {leaf, _, _}) -> not_found ;
+
 search(K, {node, T1, K2, _}) when K =< K2 -> search(K, T1) ;
 search(K, {node, _, _, T2}) -> search(K, T2).
 
@@ -46,16 +49,17 @@ search(K, {node, _, _, T2}) -> search(K, T2).
 % dict:search(7, { node, { leaf, 2, anna }, 4, { leaf, 5 , bruno} })., 
 % con esito not_found
 %
+%
 % Restituisco in output una copia dell'input dove ho cambiato l'associazione
-% chiave-valore oppure ho inserito una chiave
+% chiave-valore oppure ho inserito una chiave.
 % Implementazione dell'inserimento:
 
 insert(K, V, {leaf, K2, _}) when K =:= K2 -> {leaf, K, V} ;
-insert(K, V, {leaf, K2, V2}) when K =< K2 -> 
-    {node, {leaf, K, V}, K, {leaf, K2, V2}} ;
+
+insert(K, V, {leaf, K2, V2}) when K =< K2 -> {node, {leaf, K, V}, K, {leaf, K2, V2}} ;
 insert(K, V, {leaf, K2, V2}) -> {node, {leaf, K2, V2}, K2, {leaf, K, V}} ;
-insert(K, V, {node, T1, K2, T2}) when K =< K2 -> 
-    {node, insert(K, V, T1), K2, T2} ;
+
+insert(K, V, {node, T1, K2, T2}) when K =< K2 -> {node, insert(K, V, T1), K2, T2} ;
 insert(K, V, {node, T1, K2, T2}) -> {node, T1, K2, insert(K, V, T2)}.
 
 % Un possibile esempio di esecuzione può essere il seguente:
